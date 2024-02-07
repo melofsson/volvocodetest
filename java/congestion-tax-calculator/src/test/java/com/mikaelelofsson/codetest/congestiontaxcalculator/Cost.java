d2aa21da3248622c5892d208da426819b32900e0
Mikael Elofsson <mikaelelofsson@mikaels-macbook.local>
2024-02-07 14:47:35 +0100

    Print returned value from calculation in error message if failed test

--- java/congestion-tax-calculator/src/test/java/com/mikaelelofsson/codetest/congestiontaxcalculator/Cost.java
+++ java/congestion-tax-calculator/src/test/java/com/mikaelelofsson/codetest/congestiontaxcalculator/Cost.java
@@ -27,7 +27,6 @@ class Cost {
 
 		};
 		int cost = taxCalculator.getTax("Car", getDates(stringDates));
-
 		Assert.isTrue(cost == 60, "Expected 60. Got " + cost);
 	}
 
@@ -37,7 +36,9 @@ class Cost {
 				"2013-02-08 16:48:00", //18
 				"2013-02-08 17:49:00", //13
 		};
-		Assert.isTrue(taxCalculator.getTax("Car", getDates(stringDates)) == 31, "Expected 13 as highest value");
+
+		int cost = taxCalculator.getTax("Car", getDates(stringDates));
+		Assert.isTrue(cost == 31, "Expected 31. Got " + cost);
 	}
 
 	@Test
@@ -46,7 +47,8 @@ class Cost {
 				"2013-02-08 16:48:00", //18
 				"2013-02-08 17:48:00", //13
 		};
-		Assert.isTrue(taxCalculator.getTax("Car", getDates(stringDates)) == 18, "Expected 13 as highest value");
+		int cost = taxCalculator.getTax("Car", getDates(stringDates));
+		Assert.isTrue(cost == 18, "Expected 18. Got " + cost);
 	}
 
 	@Test
@@ -56,7 +58,8 @@ class Cost {
 				"2013-02-08 18:29:00", //8 kronor
 				"2013-02-08 18:35:00", //0 kronor
 		};
-		Assert.isTrue(taxCalculator.getTax("Car", getDates(stringDates)) == 13, "Expected 13 as highest value");
+		int cost = taxCalculator.getTax("Car", getDates(stringDates));
+		Assert.isTrue(cost == 13, "Expected 13. Got " + cost);
 	}
 	@Test
 	void highestTaxPaidWithinOneHourExtendedOutsideHourSpan() {
@@ -67,7 +70,8 @@ class Cost {
 				"2013-02-08 17:49:00", //13 kronor
 				"2013-02-08 18:29:00", //8 kronor
 		};
-		Assert.isTrue(taxCalculator.getTax("Car", getDates(stringDates)) == 31, "Expected 31 as highest value");
+		int cost = taxCalculator.getTax("Car", getDates(stringDates));
+		Assert.isTrue(cost == 31, "Expected 31. Got " + cost);
 	}
 
 	@Test
@@ -77,7 +81,8 @@ class Cost {
 				"2013-03-26 14:25:00",
 				"2013-03-28 14:07:27"
 		};
-		Assert.isTrue(taxCalculator.getTax("Car", getDates(stringDates)) == 0, "Expected 31 as highest value");
+		int cost = taxCalculator.getTax("Car", getDates(stringDates));
+		Assert.isTrue(cost == 0, "Expected 0. Got " + cost);
 	}
 
 	@Test
@@ -87,8 +92,9 @@ class Cost {
 				"2013-01-14 06:00:00",
 				"2013-01-14 06:29:00", //
 		};
-		Assert.isTrue(taxCalculator.getTax("Car", getDates(stringDates)) == 8, "Expected 31 as highest value");
-	}
+		int cost = taxCalculator.getTax("Car", getDates(stringDates));
+		Assert.isTrue(cost == 8, "Expected 8. Got " + cost);
+		}
 	@Test
 	void costTimeperiodSecond() {
 		//Detected a bug . Value should be 18 + 13 = 31;
@@ -96,8 +102,9 @@ class Cost {
 				"2013-01-14 06:30:00",
 				"2013-01-14 06:59:00", //
 		};
-		Assert.isTrue(taxCalculator.getTax("Car", getDates(stringDates)) == 13, "Expected 31 as highest value");
-	}
+		int cost = taxCalculator.getTax("Car", getDates(stringDates));
+		Assert.isTrue(cost == 13, "Expected 13. Got " + cost);
+		}
 	@Test
 	void costTimeperiodThree() {
 		//Detected a bug . Value should be 18 + 13 = 31;
@@ -105,8 +112,9 @@ class Cost {
 				"2013-01-14 07:00:00",
 				"2013-01-14 07:59:00", //
 		};
-		Assert.isTrue(taxCalculator.getTax("Car", getDates(stringDates)) == 18, "Expected 31 as highest value");
-	}
+		int cost = taxCalculator.getTax("Car", getDates(stringDates));
+		Assert.isTrue(cost == 18, "Expected 18. Got " + cost);
+		}
 	@Test
 	void costTimeperiodFour() {
 		//Detected a bug . Value should be 18 + 13 = 31;
@@ -114,24 +122,27 @@ class Cost {
 				"2013-01-14 08:00:00",
 				"2013-01-14 08:29:00", //
 		};
-		Assert.isTrue(taxCalculator.getTax("Car", getDates(stringDates)) == 13, "Expected 31 as highest value");
-	}
+		int cost = taxCalculator.getTax("Car", getDates(stringDates));
+		Assert.isTrue(cost == 13, "Expected 13. Got " + cost);
+		}
 	@Test
 	void costTimeperiodFive_LowerLimit() {
 		//Detected a bug . Value should be 18 + 13 = 31;
 		String[] stringDates = {
 				"2013-01-14 14:59:00", //
 		};
-		Assert.isTrue(taxCalculator.getTax("Car", getDates(stringDates)) == 8, "Expected 31 as highest value");
-	}
+		int cost = taxCalculator.getTax("Car", getDates(stringDates));
+		Assert.isTrue(cost == 8, "Expected 8. Got " + cost);
+		}
 	@Test
 	void costTimeperiodFive_UpperLimit() {
 		//Detected a bug . Value should be 18 + 13 = 31;
 		String[] stringDates = {
 				"2013-01-14 08:30:00", //
 		};
-		Assert.isTrue(taxCalculator.getTax("Car", getDates(stringDates)) == 8, "Expected 31 as highest value");
-	}
+		int cost = taxCalculator.getTax("Car", getDates(stringDates));
+		Assert.isTrue(cost == 8, "Expected 8. Got " + cost);
+		}
 	@Test
 	void costTimeperiodSix() {
 		//Detected a bug . Value should be 18 + 13 = 31;
@@ -139,24 +150,27 @@ class Cost {
 				"2013-01-14 15:00:00", //0
 				"2013-01-14 15:29:00", //0
 		};
-		Assert.isTrue(taxCalculator.getTax("Car", getDates(stringDates)) == 13, "Expected 31 as highest value");
-	}
+		int cost = taxCalculator.getTax("Car", getDates(stringDates));
+		Assert.isTrue(cost == 13, "Expected 13. Got " + cost);
+		}
 	@Test
 	void costTimeperiodSeven_LowerLimit() {
 		//Detected a bug . Value should be 18 + 13 = 31;
 		String[] stringDates = {
 				"2013-01-14 16:59:00", //0
 		};
-		Assert.isTrue(taxCalculator.getTax("Car", getDates(stringDates)) == 18, "Expected 31 as highest value");
-	}
+		int cost = taxCalculator.getTax("Car", getDates(stringDates));
+		Assert.isTrue(cost == 18, "Expected 18. Got " + cost);
+		}
 	@Test
 	void costTimeperiodSeven_UpperLimit() {
 		//Detected a bug . Value should be 18 + 13 = 31;
 		String[] stringDates = {
 				"2013-01-14 15:30:00", //0
 		};
-		Assert.isTrue(taxCalculator.getTax("Car", getDates(stringDates)) == 18, "Expected 31 as highest value");
-	}
+		int cost = taxCalculator.getTax("Car", getDates(stringDates));
+		Assert.isTrue(cost == 18, "Expected 18. Got " + cost);
+		}
 	@Test
 	void costTimeperiodEight() {
 		//Detected a bug . Value should be 18 + 13 = 31;
@@ -164,8 +178,9 @@ class Cost {
 				"2013-01-14 17:00:00", //0
 				"2013-01-14 17:59:00",
 		};
-		Assert.isTrue(taxCalculator.getTax("Car", getDates(stringDates)) == 13, "Expected 31 as highest value");
-	}
+		int cost = taxCalculator.getTax("Car", getDates(stringDates));
+		Assert.isTrue(cost == 13, "Expected 13. Got " + cost);
+		}
 	@Test
 	void costTimeperiodNine() {
 		//Detected a bug . Value should be 18 + 13 = 31;
@@ -173,8 +188,9 @@ class Cost {
 				"2013-01-14 18:00:00", //0
 				"2013-01-14 18:29:00",
 		};
-		Assert.isTrue(taxCalculator.getTax("Car", getDates(stringDates)) == 8, "Expected 31 as highest value");
-	}
+		int cost = taxCalculator.getTax("Car", getDates(stringDates));
+		Assert.isTrue(cost == 8, "Expected 8. Got " + cost);
+		}
 	@Test
 	void costTimeperiodTen() {
 		//Detected a bug . Value should be 18 + 13 = 31;
@@ -182,8 +198,9 @@ class Cost {
 				"2013-01-14 18:30:00", //0
 				"2013-01-14 05:59:00",
 		};
-		Assert.isTrue(taxCalculator.getTax("Car", getDates(stringDates)) == 0, "Expected 31 as highest value");
-	}
+		int cost = taxCalculator.getTax("Car", getDates(stringDates));
+		Assert.isTrue(cost == 0, "Expected 0. Got " + cost);
+		}
 
 
 
